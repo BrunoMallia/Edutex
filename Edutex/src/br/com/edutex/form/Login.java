631cbb8ca36611487f7c17eb342bb44ba9712d5c
bruno <bruno@bruno-AHV>
2015-01-08 15:17:17 -0200

    Implementação básica do log4j

--- Edutex/src/br/com/edutex/form/Login.java
+++ Edutex/src/br/com/edutex/form/Login.java
@@ -3,16 +3,25 @@ package br.com.edutex.form;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+
 public class Login extends Action {
+	
+	  private Logger log = null;
+	  
+	  
 	
 	@Override 
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
+		
+		log = Logger.getRootLogger();
+		log.error("TESTE ERROR LOG4J");
 		
 		/*LÓGICA PARA ATENTICAÇÃO DE USUÁRIO E SENHA*/
 		
